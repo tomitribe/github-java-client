c09c69fd71bf9263957af71266781844287cf73a
David Blevins <dblevins@tomitribe.com>
2025-01-21 10:57:12 -0800

    Remove generation code

--- github-client/src/test/resources/gen/header.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-P="${1?Specify a API path}"
-{
-echo -n "\"$P\": "
-cat api.github.com.json| jq ".components.headers.\"$P\""
-} | pbcopy && pbpaste
--- github-client/src/test/resources/gen/path.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-P="${1?Specify a API path}"
-{
-echo -n "\"$P\": "
-cat api.github.com.json| jq ".paths.\"$P\""
-} | pbcopy && pbpaste
