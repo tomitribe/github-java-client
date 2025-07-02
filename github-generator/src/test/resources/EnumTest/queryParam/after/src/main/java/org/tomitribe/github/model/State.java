faafcb20265383aad785d6c81c15cf1332f1cf66
David Blevins <dblevins@tomitribe.com>
2025-07-01 20:24:02 -0700

    Missing test resources

---
+++ github-generator/src/test/resources/EnumTest/queryParam/after/src/main/java/org/tomitribe/github/model/State.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tomitribe.github.model;
+
+import jakarta.json.bind.annotation.JsonbTypeAdapter;
+import org.tomitribe.github.core.EnumAdapter;
+
+@JsonbTypeAdapter(State.Adapter.class)
+public enum State {
+
+    OPEN("open"), CLOSED("closed"), ALL("all");
+
+    private final String name;
+
+    State(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static class Adapter extends EnumAdapter<State> {
+
+        public Adapter() {
+            super(State.class);
+        }
+    }
+}
